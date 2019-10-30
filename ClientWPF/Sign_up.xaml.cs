6ae8256655de1dd433313f904f67e57ffa0985ce
Z X <nekotanekota@yandex.ru>
2019-10-30 22:12:24 +0300

    Zulfiyas commit

--- ClientWPF/Sign_up.xaml.cs
+++ ClientWPF/Sign_up.xaml.cs
@@ -18,27 +18,16 @@ using System.Windows.Shapes;
 namespace ClientWPF
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Sign_up.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Sign_up : Window
     {
-        public MainWindow()
+        public Sign_up()
         {
-            InitializeComponent(); // second-commit
+            InitializeComponent();
         }
-        public void Open()
-        {
-            Account account = new Account();
-            account.Show();
-            this.Close();
-        }
-        public void Open_sp()
-        {
-            Sign_up sign = new Sign_up();
-            sign.Show();
-            this.Close();
-        }
-        private void sendToServ_Click(object sender, RoutedEventArgs e)
+
+        private void sign_up_button_Click(object sender, RoutedEventArgs e)
         {
             try
             {
@@ -49,18 +38,17 @@ namespace ClientWPF
 
                 /* Берем текст из поля и отправляем на сервер */
                 //string message = textBox.Text; //закоммитила З.
-                User user = new User();
-                user.User_name = Login.Text;                     
-                user.User_password = Password.Password;            
+                string User_name_sp = login_sp.Text;
+                string User_password_sp = password_sp.Text;
                 StreamWriter writer = new StreamWriter(stream);
-                writer.WriteLine(user.User_name);                        
-                writer.WriteLine(user.User_password);                     
+                writer.WriteLine(login_sp.Text);
+                writer.WriteLine(password_sp.Text);
                 writer.Flush(); // мгновенная отправка
 
                 /* Получаем ответ с сервера и выводим в бокс */
                 StreamReader reader = new StreamReader(stream);
                 // taked.Text = "Получен ответ: " + reader.ReadLine(); //закоммитила З.
-                Open();
+                MessageBox.Show("Вы зарегистрировались!");
                 /* Закрываем все потоки */
                 reader.Close();
                 writer.Close();
@@ -69,14 +57,8 @@ namespace ClientWPF
             catch
             {
                 //taked.Text = "Отсутствует соединение с сервером";  // закоммитила З.
-                MessageBox.Show("Неправильный логин или пароль!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);//добавила З.
+                MessageBox.Show("Такой логин уже существует!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);//добавила З.
             }
-           
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Open_sp();
         }
     }
 }
---
+++ ClientWPF/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientWPF
+{
+    class User
+    {
+        public string User_name;
+        public string User_password;
+    }
+}
